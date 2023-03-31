04509090f385890ec4e39f9e3e5dd3bffca1b4c2
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2023-03-31 09:11:33 -0300

    fix: teste em prod.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,22 @@ import { HomePage } from './HomePage'
 
 const categoryOrder = ['html', 'css', 'javascript', 'typescript', 'react.js', 'next.js', 'react native', 'node.js']
 
+async function getData({ searchParams }: { searchParams: { search: string } }): Promise<VideoList> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos?page=1&_limit=1001${searchParams.search ? `&title=${searchParams.search}` : ''}`)
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+
 export default async function PageHome({ searchParams }: { searchParams: { search: string } }) {
-  const videosList: VideoList = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos?page=1&_limit=1001${searchParams.search ? `&title=${searchParams.search}` : ''}`, { next: { revalidate: 0 } }).then((data) => data.json())
+  const videosList = await getData({ searchParams })
   // let sortedVideos: Video[] | undefined
 
   // if(videosList.data){
