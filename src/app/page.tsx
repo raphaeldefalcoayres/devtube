03cef809dc42174412036ea4936d155e59e99f9d
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2023-03-31 09:27:02 -0300

    fix: teste em prod.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import { HomePage } from './HomePage'
 const categoryOrder = ['html', 'css', 'javascript', 'typescript', 'react.js', 'next.js', 'react native', 'node.js']
 
 export default async function PageHome({ searchParams }: { searchParams: { search: string } }) {
-  const videosList: VideoList = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos?page=1&_limit=1001${searchParams.search ? `&title=${searchParams.search}` : ''}`, { next: { revalidate: 60 * 60 * 24 } }).then((data) => data.json())
+  const videosList: VideoList = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos?page=1&_limit=1001${searchParams.search ? `&title=${searchParams.search}` : ''}`).then((data) => data.json())
   let sortedVideos: Video[] | undefined
+
+  console.log('searchParams', searchParams)
 
   if(videosList.data){
     sortedVideos = videosList.data.sort((a: { category: string }, b: { category: string }) => {
